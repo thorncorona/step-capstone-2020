ee6d62ed174e7fdc8b5b6c710569f66e81ac9b2a
Jevin G <thorncorona@gmail.com>
2020-08-03 23:26:21 +0000

    check rsc

--- src/main/java/com/google/netpcapanalysis/dao/GeolocationDaoImpl.java
+++ src/main/java/com/google/netpcapanalysis/dao/GeolocationDaoImpl.java
@@ -20,7 +20,7 @@ public class GeolocationDaoImpl implements GeolocationDao {
     try {
       if (GeolocationDaoImpl.class == null) throw new Error("class null");
       if (GeolocationDaoImpl.class.getClassLoader() == null) throw new Error("no class loader");
-      if (GeolocationDaoImpl.class.getClassLoader().getResource(GEO_DB_LOCATION) == null) throw new Error("resource null");
+      if (GeolocationDaoImpl.class.getClassLoader().getResource("resources/" + GEO_DB_LOCATION) == null) throw new Error("resource null");
       URL geoDBUrl = GeolocationDaoImpl.class.getClassLoader().getResource("resources/" + GEO_DB_LOCATION);
       database = new File(geoDBUrl.toURI());
       reader = new DatabaseReader.Builder(database).build();
